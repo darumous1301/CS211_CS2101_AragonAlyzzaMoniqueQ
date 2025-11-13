7b0a74f4ad86fb007b11beff9fd0cfe712ef7bee
mnemoniques <alyxxaragon@gmail.com>
2025-11-13 09:57:38 +0800

    Add files via upload

---
+++ 02 - Laboratory Activities/Laboratory Activity 3/Artists/Painter.java
@@ -0,0 +1,31 @@
+//Instance of Painter Class
+enum Medium { 
+    OIL, 
+    WATERCOLOR, 
+    ACRYLIC, 
+    PENCIL, 
+    CHARCOAL 
+}
+
+    class Painter extends Artist {
+    private Medium medium;
+
+    public Painter(String name, int age, String specialty, Medium medium) {
+        super(name, age, specialty);
+        this.medium = medium;
+    }
+    
+    public Medium getMedium() {
+        return medium;
+    }
+
+    public void setMedium(Medium medium) {
+        this.medium = medium;
+    }
+    
+    public void displayInfo() {
+        System.out.println("Painter\'s Info:");
+        super.displayInfo();
+        System.out.println("Medium: " + medium);
+    }
+}
